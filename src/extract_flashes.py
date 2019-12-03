f9ccfcccc35683d759880db3a8c5e448d202a553
Kurtis Pinkney <kurtis.pinkney@gmail.com>
2019-12-02 22:10:52 -0500

    Added function to create dictionary of flash id's and lat/lon locations.

--- src/extract_flashes.py
+++ src/extract_flashes.py
@@ -14,7 +14,7 @@ import xarray as xr
 from typing import Dict
 
 
-def read_glm_file(glm_file: str):
+def read_glm_file(glm_file: str) -> xr.Dataset:
     if not glm_file.endswith(".nc"):
         raise ValueError
     if os.path.exists(glm_file):
@@ -22,8 +22,13 @@ def read_glm_file(glm_file: str):
     raise FileNotFoundError
 
 
-def get_flashes() -> Dict:
-    pass
+def get_flash_info(glm_data: xr.Dataset) -> Dict:
+    return {glm_data["Id"][idx]:
+            {"lat": glm_data["Lat"][idx], "lon": glm_data["Lon"][idx]}
+            for idx
+            in range(len(glm_data["Id"]))}
 
-x = read_glm_file("/Users/kurtispinkney/Desktop/WxProject/OR_GLM-L2-LCFA_G16_s20193002359400_e20193010000000_c20193010000028.nc")
-print(x["flash_lat"].data)
+
+
+# x = read_glm_file("/Users/kurtispinkney/Desktop/WxProject/OR_GLM-L2-LCFA_G16_s20193002359400_e20193010000000_c20193010000028.nc")
+# print(x["flash_lat"].data)
